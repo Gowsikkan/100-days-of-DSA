820acc4dadc23092fc2ebe4651f93e72c4b108f7
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2021-08-02 20:07:49 +0530

    Array

--- Day-1/Array-1.py
+++
@@ -1,9 +0,0 @@
-n = int(input("sdfgh"))
-arr = []
-for i in range(0,n):
-    v = int(input())
-    arr.append(v)
-
-for i in range(0,n):
-    print(arr[i])
-
---
+++ Day-1/Problem-001.py
@@ -0,0 +1,23 @@
+class Solution:
+    def createTargetArray(self,nums,index):
+        s=[]
+        for i in range(len(nums)):
+            s.insert(index[i],nums[i])
+        return s
+    
+
+n=int(input("Enter size : "))
+
+nums = []
+index = []
+
+for i in range(n):
+    c=int(input("Enter values :"))
+    nums.append(c)
+
+for i in range(n):
+    c=int(input("Enter index :"))
+    index.append(c)
+
+print(Solution().createTargetArray(nums,index))
+
