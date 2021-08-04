811d689083aaada7a429b46d90b3c019bfb8589f
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2021-08-04 10:34:14 +0530

    Array-3

---
+++ Day-3/Problem-003.py
@@ -0,0 +1,23 @@
+#Kids With the Greatest Number of Candies
+class Solution:
+    def kidsWithCandies(self, candies,extraCandies):
+        c=list()
+        for i in candies:
+            if i+extraCandies>=max(candies):
+                c.append(True)
+            else:
+                c.append(False)
+        return c
+
+candies =[]
+n=int(input("Enter size :"))
+
+for i in range(n):
+    e=int(input())
+    candies.append(e)
+
+extraCandies = int(input("Extra"))
+
+print(Solution().kidsWithCandies(candies,extraCandies))
+
+                
