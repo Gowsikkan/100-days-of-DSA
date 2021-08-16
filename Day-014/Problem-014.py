e29a32a32bc80adafa51a0e0f263c4c19e409499
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2021-08-16 10:23:11 +0530

    String

---
+++ Day-014/Problem-014.py
@@ -0,0 +1,20 @@
+#Keyboard Row
+
+class Solution:
+    def findWords(self, words):
+        word_list=[]
+        top_row=set('qwertyuiop')
+        mid_row=set('asdfghjkl')
+        bottom_row=set('zxcvbnm')
+        for word in words:
+            if set(word.lower()).issubset(top_row) or set(word.lower()).issubset(mid_row) or set(word.lower()).issubset(bottom_row):
+                word_list.append(word)
+        return word_list
+
+n=int(input("Enter size: "))
+c=[]
+for i in range(n):
+    s=input()
+    c.append(s)
+
+print(Solution().findWords(c))
