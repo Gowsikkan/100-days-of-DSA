bf6c1e6c74b3686e253e75a9fa35ab685456c0cc
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2021-09-02 00:42:44 +0530

    Stack

---
+++ Day-030/Project-030.py
@@ -0,0 +1,27 @@
+#Baseball Game
+
+class Solution:
+    def calPoints(self, ops):
+        s=[]
+        c=-1
+        for i in ops:
+            if i =="D":
+                s.append(s[c]*2)
+                c+=1
+            elif i=="C":
+                s.pop()
+                c-=1
+            elif i=="+":
+                s.append(s[c]+s[c-1])
+                c+=1
+            else:
+                s.append(int(i))
+                c+=1
+        return sum(s)
+
+n=int(input("Enter size : "))
+s=[]
+for i in range(n):
+    s.append(input())
+
+print("Sum : ",Solution().calPoints(s))
