c5289b0d64db7b0fb109d0a32d1b98dbb8fd2478
Gowsikkan <66643168+Gowsikkan@users.noreply.github.com>
2021-09-27 23:38:32 +0530

    Queue

--- Day-037/Problem-037.py
+++ Day-037/Problem-037.py
@@ -1,1 +1,18 @@
-#hlo
+def twosum(num,target):
+    x=[]
+    for i in range(len(num)):
+        for j in range(len(num)):
+            if i!=j:
+                if num[i]+num[j]==target:
+                    x.append([i,j])
+        break
+    return x
+
+a=int(input("enter the size of st2ring"))
+num=[]
+for i in range(a):
+    b=int(input("enter the value"))
+    num.append(b)
+target=int(input("enter the target num"))
+x=twosum(num,target)
+print(x)
